f1b1098893d146b1d2747d754739a81de9698a2b
Chao Wang, Vodafone Australia <chao.wang@vodafone.com.au>
2021-11-23 15:07:45 +1100

    create screens with placeholder text

---
+++ src/screens/LoginScreen.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import {Text} from 'react-native';
+
+const LoginScreen: React.FC<{}> = () => {
+  return <Text>Login screen</Text>;
+};
+
+export default LoginScreen;
---
+++ src/screens/MfaScreen.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import {View} from 'react-native';
+
+const MfaScreen: React.FC<{}> = () => {
+  return <View>Mfa Screen </View>;
+};
+
+export default MfaScreen;
