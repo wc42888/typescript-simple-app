b2cf54b7711d8a7f81337fea6ef946201255e254
Chao Wang, Vodafone Australia <chao.wang@vodafone.com.au>
2021-11-23 15:06:50 +1100

    added react-navigation to the project

--- android/app/src/main/java/com/med/MainActivity.java
+++ android/app/src/main/java/com/med/MainActivity.java
@@ -1,6 +1,7 @@
 package com.med;
 
 import com.facebook.react.ReactActivity;
+import android.os.Bundle;
 
 public class MainActivity extends ReactActivity {
 
@@ -11,5 +12,13 @@ public class MainActivity extends ReactActivity {
   @Override
   protected String getMainComponentName() {
     return "med";
+  }
+
+  /**
+   * react-native-screens package setup
+   */
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(null);
   }
 }
