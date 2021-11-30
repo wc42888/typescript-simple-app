5f8054023c9b98ad2b251d9943140d28f46f6e76
Chao Wang, Vodafone Australia <chao.wang@vodafone.com.au>
2021-11-30 15:01:08 +1100

    added input component to login page

--- src/screens/LoginScreen.tsx
+++ src/screens/LoginScreen.tsx
@@ -1,8 +1,31 @@
-import React from 'react';
-import {Text} from 'react-native';
+import React, {useState} from 'react';
+import {useForm} from 'react-hook-form';
+import {Input} from '@components/index';
 
 const LoginScreen: React.FC<{}> = () => {
-  return <Text>Login screen</Text>;
+  const {control, clearErrors} = useForm({
+    mode: 'onBlur',
+  });
+
+  return (
+    <>
+      <Input
+        placeholder="Username"
+        control={control}
+        name="username"
+        rules={{
+          required: 'username is required',
+        }}
+        clearErrors={clearErrors}
+      />
+      <Input
+        placeholder="Password"
+        control={control}
+        name="password"
+        clearErrors={clearErrors}
+      />
+    </>
+  );
 };
 
 export default LoginScreen;
