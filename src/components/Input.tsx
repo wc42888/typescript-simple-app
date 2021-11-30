ce3fb78cf2d80a4c745f4d98fc09089e831a295c
Chao Wang, Vodafone Australia <chao.wang@vodafone.com.au>
2021-11-30 15:00:55 +1100

    added an input component

---
+++ src/components/Input.tsx
@@ -0,0 +1,64 @@
+import React, {FC, useState} from 'react';
+import styled from 'styled-components/native';
+import {Text, TextInputProps} from 'react-native';
+import {useController, UseControllerProps} from 'react-hook-form';
+import {LIGHT_GREY, GREEN} from '@config/colors';
+import {FONT_SIZE_NORMAL} from '@config/fonts';
+
+interface IStyledInputProps extends TextInputProps {
+  isActive: boolean;
+}
+
+interface IUseFormInputProps {
+  clearErrors: (name?: string | string[]) => void;
+}
+
+const StyledInput = styled.TextInput<IStyledInputProps>`
+  border-bottom-width: 1px;
+  border-bottom-color: ${({isActive}) => (isActive ? GREEN : LIGHT_GREY)};
+  height: 50px;
+  font-size: ${FONT_SIZE_NORMAL};
+`;
+
+const Input: FC<UseControllerProps & TextInputProps & IUseFormInputProps> = ({
+  control,
+  name,
+  rules,
+  clearErrors,
+  ...inputProps
+}) => {
+  const {field, fieldState} = useController({
+    control,
+    name,
+    rules,
+  });
+
+  const [isActive, setIsActive] = useState<boolean>(false);
+
+  const onInputFocus = () => {
+    clearErrors(name);
+    setIsActive(true);
+  };
+
+  const onInputBlur = () => {
+    field.onBlur();
+    setIsActive(false);
+  };
+
+  return (
+    <>
+      <StyledInput
+        {...inputProps}
+        isActive={isActive}
+        onFocus={onInputFocus}
+        onBlur={onInputBlur}
+        value={field.value}
+        onChangeText={field.onChange}
+        autoCapitalize="none"
+      />
+      <Text>{fieldState?.error?.message}</Text>
+    </>
+  );
+};
+
+export default Input;
