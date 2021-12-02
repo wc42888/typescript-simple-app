5ce500066f60a76d95b6880b5903b41bb85c186b
chao wang <>
2021-12-02 11:18:03 +1100

    added a SafeAreaView on root navigator

--- src/navigation/RootNavigator.tsx
+++ src/navigation/RootNavigator.tsx
@@ -1,11 +1,18 @@
 import React from 'react';
 import {NavigationContainer} from '@react-navigation/native';
+import styled from 'styled-components/native';
 import LoginStack from './LoginStack';
 
+const Container = styled.SafeAreaView`
+  flex: 1;
+`;
+
 const RootNavigator = () => (
-  <NavigationContainer>
-    <LoginStack />
-  </NavigationContainer>
+  <Container>
+    <NavigationContainer>
+      <LoginStack />
+    </NavigationContainer>
+  </Container>
 );
 
 export default RootNavigator;
