80ad2c00afd1bd7526e6bbc051a5299630b9d5ea
chao wang <>
2021-12-02 12:17:24 +1100

    added new colors

--- src/config/colors.tsx
+++ src/config/colors.tsx
@@ -1,2 +1,6 @@
 export const LIGHT_GREY = '#D3D3D3';
 export const GREEN = '#5cb85c';
+export const RED = '#d9534f';
+export const BLUE = '#0275d8';
+export const TEAL = '#5bc0de';
+export const WHITE = '#FFFFFF';
