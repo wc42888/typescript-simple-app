e01e04d657951452b75523c82302bf8d2deba2a2
chao wang <>
2021-12-16 09:01:24 +1100

    connected the home screen to redux

--- src/screens/HomeScreen.tsx
+++ src/screens/HomeScreen.tsx
@@ -1,10 +1,35 @@
 import React from 'react';
-import {View, Text} from 'react-native';
+import {View, Text, StyleSheet} from 'react-native';
+import {useDispatch, useSelector} from 'react-redux';
+import {RootState, AppDispatch} from '@redux/store';
+import {setMessage} from '@redux/messageSlice';
+import Button from '@components/Button';
+import {BLUE} from '@config/colors';
+
+const styles = StyleSheet.create({
+  button: {
+    marginTop: 20,
+    borderRadius: 5,
+    backgroundColor: BLUE,
+    height: 50,
+    width: 100,
+  },
+});
 
 const HomeScreen = () => {
+  const dispatch = useDispatch<AppDispatch>();
+  const {message} = useSelector((state: RootState) => state.message);
+
+  const dispatchChangeText = () => dispatch(setMessage('changed message'));
+
   return (
     <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
-      <Text>Home</Text>
+      <Text>{message}</Text>
+      <Button
+        buttonLabel="change text"
+        onClick={dispatchChangeText}
+        style={styles.button}
+      />
     </View>
   );
 };
