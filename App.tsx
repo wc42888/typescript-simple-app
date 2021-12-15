97cec492633e6e6cc88ae587cedf765788fef71f
chao wang <>
2021-12-16 09:01:10 +1100

    added provider to app

--- App.tsx
+++ App.tsx
@@ -9,8 +9,14 @@
  */
 
 import React from 'react';
+import {Provider} from 'react-redux';
+import store from '@redux/store';
 import RootNavigator from '@navigation/RootNavigator';
 
-const App = () => <RootNavigator />;
+const App = () => (
+  <Provider store={store}>
+    <RootNavigator />
+  </Provider>
+);
 
 export default App;
